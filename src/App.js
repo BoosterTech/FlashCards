f031f5bd3bebef5b9e15f37a230494b7929c5a88
Darek Podczasik <boostertech@mail.com>
2024-06-27 21:16:24 +0100

    handleClick() added to show answer when question clicked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import questions from "./questions";
 
 function App() {
+  const [questionId, setQuestionId] = useState(null);
+
+  const handleClick = (id) => {
+    setQuestionId(id);
+  };
+
   return (
     <div>
       {questions.map((question) => (
-        <div key={question.id}>{question.question}</div>
+        <div key={question.id} onClick={() => handleClick(question.id)}>
+          {questionId === question.id ? question.answer : question.question}
+        </div>
       ))}
     </div>
   );
