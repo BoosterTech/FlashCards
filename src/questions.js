14b95f6aed58fce03e4b529d2fbf96a86aee4eae
Darek Podczasik <boostertech@mail.com>
2024-06-27 20:46:21 +0100

    cards content array created (questions.js)

---
+++ src/questions.js
@@ -0,0 +1,35 @@
+const questions = [
+  {
+    id: 3457,
+    question: "what language i React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the bilding blocks of react apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "how to pass data from parent to child components in React?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory in React?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with the React state?",
+    answer: "Controlled element",
+  },
+];
+
+export default questions;
